5084ceb05cc17c6c9dc3a38f101ffdf584ed4ffd
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2025-01-22 11:23:01 +0900

    25.01.22 : 회원 기능 Test

---
+++ JPA_Application1/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+        // when
+        Long savedId = memberService.join(member);
+        // then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외_테스트() {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        IllegalStateException exception = assertThrows(
+                IllegalStateException.class,
+                () -> memberService.join(member2) // 예외 발생 예상
+        );
+
+        assertEquals("이미 존재하는 회원입니다.", exception.getMessage()); // 예외 메시지 검증
+    }
+
+
+}
