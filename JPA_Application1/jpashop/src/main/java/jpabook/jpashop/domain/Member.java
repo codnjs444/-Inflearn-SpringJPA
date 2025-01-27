7466f0cbe22d20584062e066d1f80a93d55f66ec
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2025-01-27 12:26:42 +0900

    25.01.27 : Entity 노출 조회문 (권장x)

--- JPA_Application1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ JPA_Application1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -25,6 +26,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
