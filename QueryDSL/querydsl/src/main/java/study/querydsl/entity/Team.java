9e628ae96a6f08b3956f2041f083792a640a22d1
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2025-04-29 18:14:42 +0900

    init

---
+++ QueryDSL/querydsl/src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,25 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
